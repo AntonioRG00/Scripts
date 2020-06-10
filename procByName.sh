5186a4a551f9452b4360b6eaaed7d1bfb973797b
Antonio Rodríguez Gundermann <60214254+AntonioRG00@users.noreply.github.com>
2020-06-10 18:04:42 +0200

    Add files via upload

---
+++ procByName.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Array with the id of the process to kill
+targets=($(ps -e | egrep -w $1 | cut -d " " -f3))
+
+#Array with the data of the process to kill
+data_targets=($(ps -e | egrep -w $1))
+
+#Size of the array to iterate
+length=${#targets[@]}
+
+#Loop for every process founded
+for((i = 0 ; i < length ; i++)); do
+
+	#Convert the process String to int
+	int=$(printf '%d' ${targets[i]})
+
+	#Loop for the data of the process
+	echo "Process to kill: '${data_targets[i]}'"
+	for((j = 0 ; j < 4 ; j++)); do
+		printf "'${data_targets[j]}' "
+	done
+	echo #Newline
+
+	#Kill the process
+	kill -9 $int
+	echo "Target finalized"
+done
+
